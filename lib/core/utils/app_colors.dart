8003a815822042827e33c6da1c19d3642d42f51c
Maheresio <zamalek.545r5@gmail.com>
2025-03-23 16:20:49 +0200

    Add color constants to AppColors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/material.dart' show Color;
 
 class AppColors {
-
+  static const kWhite = Color(0xFFFFFFFF);
+  static const kBlack = Color(0xFF222222);
+  static const kLightBlack = Color(0xFF2D2D2D);          //textfield words
+  static const kGrey = Color(0xFF9B9B9B);
+  static const kPrimary = Color(0xFFDB3022);
+  static const kErrorColor = Color(0xFFF01F0E);
+  static const kBackgroundColor = Color(0xFFF9F9F9);
+  static const kSuccessColor = Color(0xFF2AA952);
 }
+
+
+// fontFamily='Metropolis'
