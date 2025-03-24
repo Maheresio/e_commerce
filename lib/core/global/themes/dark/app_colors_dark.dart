e14547b7462ac333ed112ac78d4e23a2c8a03d75
Maheresio <zamalek.545r5@gmail.com>
2025-03-25 01:34:13 +0200

    Add dark theme colors

---
+++ lib/core/global/themes/dark/app_colors_dark.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' show Color;
+
+class AppColorsDark {
+  static const kBlack = Color(0xFF222222);
+  static const kLightWhite = Color(0xFFFDFDFD); //textfield words
+  static const kGrey = Color(0xFF9B9B9B);
+  static const kPrimary = Color(0xFFDB3022);
+  static const kErrorColor = Color(0xFFF01F0E);
+  static const kBackgroundColor = Color(0xFF292929);
+  static const kSuccessColor = Color(0xFF2AA952);
+}
+
+
+// fontFamily='Metropolis'
