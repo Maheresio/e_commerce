07c81ffeea0d974d195630aededf6f4cfb3788e9
Maheresio <zamalek.545r5@gmail.com>
2025-03-25 01:34:50 +0200

    Add SVG asset paths for new icons in AppImages

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,7 @@
 class AppImages {
-
+  static const String arrowRight = 'assets/svg/arrow.svg';
+  static const String check = 'assets/svg/check.svg';
+  static const String facebook = 'assets/svg/facebook.svg';
+  static const String google = 'assets/svg/google.svg';
+  static const String wrong = 'assets/svg/wrong.svg';
 }
