814a67b9556320596075d6654ce2ed1bd9701dc5
Maheresio <zamalek.545r5@gmail.com>
2025-03-25 01:38:32 +0200

    Add styled social button comonent

---
+++ lib/features/auth/register/presentation/widgets/styled_social_button.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce/core/extensions/theme_color.extension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class StyledSocialButton extends StatelessWidget {
+  const StyledSocialButton({super.key, required this.image, this.onTap});
+
+  final String image;
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 85,
+      width: 100.w,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+        elevation: .5,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(25),
+          onTap: onTap,
+          splashColor: context.color.primary.withValues(alpha: 0.1),
+          child: Center(child: SvgPicture.asset(image)),
+        ),
+      ),
+    );
+  }
+}
