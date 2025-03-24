f2d7442eac12748028afe348c45c2e3028c33150
Maheresio <zamalek.545r5@gmail.com>
2025-03-25 01:38:50 +0200

    Add social section widget for social account sign-up

---
+++ lib/features/auth/register/presentation/widgets/social_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_images.dart';
+import '../../../../../core/utils/app_strings.dart';
+import '../../../../../core/utils/app_styles.dart';
+import 'styled_social_button.dart';
+
+class SocialSection extends StatelessWidget {
+  const SocialSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 12,
+      children: [
+        Text(
+          AppStrings.kSignUpWithSocialAccount,
+          style: AppStyles.text14Medium,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 16,
+          children: [
+            StyledSocialButton(image: AppImages.google, onTap: () {}),
+            StyledSocialButton(image: AppImages.facebook, onTap: () {}),
+          ],
+        ),
+      ],
+    );
+  }
+}
