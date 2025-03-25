1b0b9bacd52c332a4c59b8078ddb92426e731056
Maheresio <zamalek.545r5@gmail.com>
2025-03-25 03:04:24 +0200

    Add login and register route to navigation

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,21 @@
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/login/presentation/views/login_view.dart';
 import '../../features/auth/register/presentation/views/register_view.dart';
 
 abstract class AppRouter {
-  static const kRegister = '/';
+  static const kRegister = '/register';
+  static const kLogin = '/kLogin';
 
   static final GoRouter router = GoRouter(
+    initialLocation: kLogin,
     routes: [
+      GoRoute(
+        path: kLogin,
+        builder: (context, state) {
+          return LoginView();
+        },
+      ),
       GoRoute(
         path: kRegister,
         builder: (context, state) {
