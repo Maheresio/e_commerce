d1b8e0f0fc8c9af0ec368aea238260276b41ea59
Maheresio <zamalek.545r5@gmail.com>
2025-04-20 02:55:12 +0200

    Implement LoginBloc with event handling for login process and state management

--- lib/features/auth/login/presentation/bloc/login_bloc.dart
+++ lib/features/auth/login/presentation/bloc/login_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../domain/usecases/login_with_email_and_password_use_case.dart';
+
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+  final LoginWithEmailAndPasswordUseCase loginWithEmailAndPasswordUseCase;
+
+  LoginBloc(this.loginWithEmailAndPasswordUseCase) : super(LoginInitial()) {
+    on<LoginButtonPressed>((event, emit) async {
+      emit(LoginLoading());
+      final result = await loginWithEmailAndPasswordUseCase.execute(
+        email: event.email,
+        password: event.password,
+      );
+      result.fold(
+        (failure) => emit(LoginFailure(failure.message)),
+        (user) => emit(LoginSuccess()),
+      );
     });
   }
 }
--- lib/features/auth/login/presentation/bloc/login_state.dart
+++ lib/features/auth/login/presentation/bloc/login_state.dart
@@ -1,9 +1,26 @@
 part of 'login_bloc.dart';
 
 abstract class LoginState extends Equatable {
-  const LoginState();  
+  const LoginState();
 
   @override
   List<Object> get props => [];
 }
+
 class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LoginFailure extends LoginState {
+  const LoginFailure(this.message);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+
+
