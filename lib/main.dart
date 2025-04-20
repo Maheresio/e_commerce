9b40102271c79013a31e1ca910701554dc580d36
Maheresio <zamalek.545r5@gmail.com>
2025-04-20 03:08:32 +0200

    Refactor main.dart to improve theme imports and enhance focus management in GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-import 'core/global/themes/dark/dark_theme.dart';
-import 'core/global/themes/light/light_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/global/themes/dark/dark_theme.dart';
+import 'core/global/themes/light/light_theme.dart';
+import 'core/services/firebase_init.dart';
+import 'core/services/service_locator.dart';
 import 'core/utils/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await firebaseInit();
+  serviceLocator();
 
   runApp(MyApp());
 }
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
           child: MaterialApp.router(
             theme: lightTheme,
             darkTheme: darkTheme,
