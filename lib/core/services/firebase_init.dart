9e0edf2234f76e95c038c442b9888fe76e339cf6
Maheresio <zamalek.545r5@gmail.com>
2025-04-20 03:10:39 +0200

    Refactor import statements in firebase_init.dart and login_repository_impl.dart for improved readability

--- lib/core/services/firebase_init.dart
+++ lib/core/services/firebase_init.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+
 import '../../firebase_options.dart';
 
 Future<void> firebaseInit() async => await Firebase.initializeApp(
--- lib/features/auth/login/data/repositories/login_repository_impl.dart
+++ lib/features/auth/login/data/repositories/login_repository_impl.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
-import 'package:e_commerce/core/error/failure.dart';
-import 'package:e_commerce/core/error/firebase_failure.dart';
-import 'package:e_commerce/core/error/socket_failure.dart';
-import 'package:e_commerce/features/auth/login/data/datasources/login_data_source.dart';
-import 'package:e_commerce/features/auth/login/domain/repositories/login_repository.dart';
+import '../../../../../core/error/failure.dart';
+import '../../../../../core/error/firebase_failure.dart';
+import '../../../../../core/error/socket_failure.dart';
+import '../datasources/login_data_source.dart';
+import '../../domain/repositories/login_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginRepositoryImpl implements LoginRepository {
