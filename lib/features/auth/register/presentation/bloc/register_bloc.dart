4db8eb330925f4770c3c9daad68ef52286e55e6c
Maheresio <zamalek.545r5@gmail.com>
2025-04-20 02:54:06 +0200

    Implement RegisterBloc with event handling for user registration and state management

--- lib/features/auth/register/presentation/bloc/register_bloc.dart
+++ lib/features/auth/register/presentation/bloc/register_bloc.dart
@@ -1,13 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../domain/usecases/register_with_email_and_password_usecase.dart';
+
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
+  final RegisterWithEmailAndPasswordUsecase registerWithEmailAndPasswordUsecase;
+  RegisterBloc(this.registerWithEmailAndPasswordUsecase)
+    : super(RegisterInitial()) {
+    on<RegisterButtonPressed>((event, emit) async {
+      emit(RegisterLoading());
+      final result = await registerWithEmailAndPasswordUsecase.execute(
+        email: event.email,
+        name: event.name,
+        password: event.password,
+      );
+      result.fold(
+        (failure) {
+          emit(RegisterFailure(failure.message));
+        },
+        (success) {
+          emit(RegisterSuccess());
+        },
+      );
     });
   }
 }
--- lib/features/auth/register/presentation/bloc/register_event.dart
+++ lib/features/auth/register/presentation/bloc/register_event.dart
@@ -6,3 +6,16 @@ abstract class RegisterEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class RegisterButtonPressed extends RegisterEvent {
+  final String email;
+  final String name;
+  final String password;
+  const RegisterButtonPressed({
+    required this.email,
+    required this.name,
+    required this.password,
+  });
+  @override
+  List<Object> get props => [email, name, password];
+}
