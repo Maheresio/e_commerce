714c29c38ffbfdc096a771fa1e76fbad4598fc62
Maheresio <zamalek.545r5@gmail.com>
2025-04-20 03:07:19 +0200

    Refactor AppRouter to include BlocProviders for Login and Register views

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/login/presentation/bloc/login_bloc.dart';
 import '../../features/auth/login/presentation/views/login_view.dart';
+import '../../features/auth/register/presentation/bloc/register_bloc.dart';
 import '../../features/auth/register/presentation/views/register_view.dart';
+import '../../features/home/presentation/views/home_view.dart';
+import '../services/service_locator.dart';
 
 abstract class AppRouter {
   static const kRegister = '/register';
   static const kLogin = '/kLogin';
+  static const kHome = '/home';
 
   static final GoRouter router = GoRouter(
     initialLocation: kLogin,
@@ -13,15 +19,22 @@ abstract class AppRouter {
       GoRoute(
         path: kLogin,
         builder: (context, state) {
-          return LoginView();
+          return BlocProvider(
+            create: (context) => sl<LoginBloc>(),
+            child: LoginView(),
+          );
         },
       ),
       GoRoute(
         path: kRegister,
         builder: (context, state) {
-          return RegisterView();
+          return BlocProvider(
+            create: (context) => sl<RegisterBloc>(),
+            child: RegisterView(),
+          );
         },
       ),
+      GoRoute(path: kHome, builder: (context, state) => HomeView()),
     ],
   );
 }
