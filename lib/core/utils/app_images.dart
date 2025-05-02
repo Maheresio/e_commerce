3e876d08cecf90d2f0f8d6082f4c54402625492c
Maheresio <zamalek.545r5@gmail.com>
2025-05-02 09:33:20 +0300

    Add home banner image path to AppImages class

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -4,4 +4,5 @@ class AppImages {
   static const String facebook = 'assets/svg/facebook.svg';
   static const String google = 'assets/svg/google.svg';
   static const String wrong = 'assets/svg/wrong.svg';
+  static const String homeBanner = 'assets/images/home_banner.png';
 }
