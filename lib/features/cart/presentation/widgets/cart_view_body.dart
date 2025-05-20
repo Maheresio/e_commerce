cc55d14a562b8eb97ccaf889fb9825aa4bf41d5c
Maheresio <zamalek.545r5@gmail.com>
2025-05-20 22:24:36 +0300

    Refactor CartPromoField layout and update CartViewBody structure

--- lib/features/cart/presentation/widgets/cart_view_body.dart
+++ lib/features/cart/presentation/widgets/cart_view_body.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/utils/app_strings.dart';
+import '../../../../core/widgets/summary_price.dart';
 import 'cart_header.dart';
 import 'cart_list_view.dart';
 import 'cart_promo_field.dart';
+import 'checkout_button.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
@@ -15,18 +18,24 @@ class CartViewBody extends StatelessWidget {
       child: CustomScrollView(
         physics: BouncingScrollPhysics(),
         slivers: [
-          SliverToBoxAdapter(
-            child: CartHeader(),
-          ),
+          SliverToBoxAdapter(child: CartHeader()),
 
           CartListView(),
 
           SliverToBoxAdapter(
-            child: CartPromoField(),
+            child: Column(
+              spacing: 15,
+              children: [
+                SizedBox(height: 5),
+                CartPromoField(),
+                SummaryPriceTile(price: 50, title: AppStrings.kTotalAmount),
+
+                CheckoutButton(),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
-
