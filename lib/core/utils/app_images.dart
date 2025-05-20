60eda24967020c4e8e462c02cdd4d8f1fd8b7c22
Maheresio <zamalek.545r5@gmail.com>
2025-05-20 22:19:59 +0300

    Add new payment and shipping SVGs and images
    - Added new SVG files for payment methods: Mastercard, Chip, FedEx, DHL, and Stripe.
    - Added new PNG images for payment methods: FedEx, PayPal, USPS, Stripe, and Visa.
    - Updated AppImages class to include paths for the newly added images and SVGs.

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -4,6 +4,14 @@ class AppImages {
   static const String facebook = 'assets/svg/facebook.svg';
   static const String google = 'assets/svg/google.svg';
   static const String wrong = 'assets/svg/wrong.svg';
+  static const String mastercard = 'assets/svg/mastercard.svg';
+  static const String chip = 'assets/svg/chip.svg';
+  static const String fedex = 'assets/images/fedex.png';
+  static const String paypal = 'assets/images/paypal.png';
+  static const String usps = 'assets/images/usps.png';
+  static const String stripe = 'assets/images/stripe.png';
+  static const String dhl = 'assets/svg/dhl.svg';
+  static const String visa = 'assets/images/visa.png';
   static const String homeBanner = 'assets/images/home_banner.png';
   static const String photo = 'assets/images/photo.png';
 }
